25f98187ef7676215a1bd759207cd82e3ff24ce6
Renan <renanymz@gmail.com>
2024-03-04 22:23:35 -0300

    Signed-off-by: Renan <renanymz@gmail.com>

---
+++ DiagramaClasseJogo/Player.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Entity import Entity
+
+class Player(Entity):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
+
+    def Operation2(self, ):
+        pass
+
